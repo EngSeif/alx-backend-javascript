bbd3f9c30827b5fe01d8dd464bdec39af5def08e
EngSeif <eng.seifmohamed2004@gmail.com>
2024-10-04 02:59:58 +0300

    Classes | Fix Code edit

--- 0x02-ES6_classes/9-hoisting.js
+++ 0x02-ES6_classes/9-hoisting.js
@@ -11,30 +11,28 @@ export class HolbertonClass {
   get location() {
     return this._location;
   }
-
 }
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-
 export class StudentHolberton {
-    constructor(firstName, lastName, holbertonClass) {
-        this._firstName = firstName;
-        this._lastName = lastName;
-        this._holbertonClass = holbertonClass;
-    }
-    
-    get fullName() {
-        return `${this._firstName} ${this._lastName}`;
-    }
-    
-    get holbertonClass() {
-        return this.holbertonClass;
-    }
-    
-    get fullStudentDescription() {
-        return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
+  constructor(firstName, lastName, holbertonClass) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._holbertonClass = holbertonClass;
+  }
+
+  get fullName() {
+    return `${this._firstName} ${this._lastName}`;
+  }
+
+  get holbertonClass() {
+    return this._holbertonClass;
+  }
+
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
 }
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
